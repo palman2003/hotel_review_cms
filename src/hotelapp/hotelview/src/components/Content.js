0cde995b87e831dfec59ad1e1946358c96d2311a
palman2003 <palayilmanav@gmail.com>
2023-07-10 22:30:28 +0530

    first commit

---
+++ src/hotelapp/hotelview/src/components/Content.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import React, { useEffect , useState } from 'react';
+
+export const Content = (items) => {
+    
+const [data,setHotels]=useState([])
+useEffect(()=>{
+    const fetchData = async ()=>{
+        try {
+        const res = await axios.get('http://localhost:1337/api/restaurants',
+        {
+            headers:{
+                Authorization:"bearer"+process.env.REACT_APP_API_TOKEN,
+            }
+        });
+        setHotels(res.data.data);
+        }
+        catch(error){
+            console.log(error);
+        }
+
+    };
+    fetchData();
+},[]);
+
+  return (
+    <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap w-full mb-20">
+      <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Information About Your Favourite Cuisines and Restaurants</h1>
+        <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+      </div>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Some features that a hotel review system app can have include a review system that allows users to rate and review hotels, an intuitive and easy-to-use interface, and more</p>
+    </div>
+    <div className="flex flex-wrap -m-4">
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{}</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content"/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content"/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+      <div className="xl:w-1/4 md:w-1/2 p-4">
+        <div className="bg-gray-100 p-6 rounded-lg">
+          <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content"/>
+          <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+          <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
+          <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  );
+}
+
+
+export default Content;
